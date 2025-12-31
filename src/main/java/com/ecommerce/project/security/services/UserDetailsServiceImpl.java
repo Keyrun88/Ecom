f6c1f9a114507a1cce420a48116ca79e38d02c01
keyrun <kiranpatil1999@outlook.com>
2025-12-31 13:32:50 +0530

    final commit

--- src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
@@ -1,8 +1,6 @@
 package com.ecommerce.project.security.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,14 +8,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ecommerce.project.model.User;
-import com.ecommerce.project.repositories.UserRepository;new ApiException("User Not Found with username: " + username);new ApiException("User Not Found with username: " + username);
+import com.ecommerce.project.repositories.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUserName(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User Not Found with username: " + username));
 
         return UserDetailsImpl.build(user);
     }
-
-
 }
